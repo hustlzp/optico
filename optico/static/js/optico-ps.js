de32210bd0f96273afc3d5b89b9d9117e46f082d
hustlzp <hustlzp@qq.com>
2013-04-24 11:07:57 +0800

    optimize the js code for showing tree list of products

--- optico/static/js/optico-ps.js
+++ optico/static/js/optico-ps.js
@@ -1,34 +1,48 @@
-function init_ps(level, mtype_id, stype_id, p_id){
-	if(level >= 1){
-		$('.ps-mtype').each(function(index, mt){
-			if($(this).attr('data-mtype-id') == mtype_id){
-				$(this).find('.li-header').removeClass('li-header-right').addClass('li-header-down');
-				$(this).next().show();
+function init_ps(level, id){
+	if(level == 1){
+		$('.ps-lv1').each(function(index, mt){
 
-				if(level == 1){
-					$(this).find('a').css('color', 'red');
-				}
+			if($(this).attr('data-id') == id){
+				// hilight itself
+				$(this).children('a').css('color', 'red');
+
+				// show level 2 list
+				$(this).children('.li-header').removeClass('li-header-right').addClass('li-header-down');
+				$(this).children('ul').show();
 			}
 		});
 	}
 
-	if(level >= 2){
-		$('.ps-stype').each(function(index, st){
-			if($(this).attr('data-stype-id') == stype_id){
-				$(this).find('.li-header').removeClass('li-header-right').addClass('li-header-down');
-				$(this).next().show();
+	if(level == 2){
+		$('.ps-lv2').each(function(index, st){
+			if($(this).attr('data-id') == id){
+				// hilight itself
+				$(this).children('a').css('color', 'red');
 
-				if(level == 2){
-					$(this).find('a').css('color', 'red');
-				}
+				// show level 3 list
+				$(this).children('.li-header').removeClass('li-header-right').addClass('li-header-down');
+				$(this).children('ul').show();
+				
+				// show level 2 list
+				$(this).parents('.ps-lv2-list').prev().prev().removeClass('li-header-right').addClass('li-header-down');
+				$(this).parents('.ps-lv2-list').show();
 			}
 		});
 	}
 
 	if(level == 3){
-		$('.ps-product').each(function(index, p){
-			if($(this).attr('data-p-id') == p_id){
-				$(this).find('a').css('color', 'red');
+		$('.ps-lv3').each(function(index, p){
+			if($(this).attr('data-id') == id){
+				// hilight itself
+				$(this).children('a').css('color', 'red');
+
+				// show level 3 list
+				$(this).parents('.ps-lv3-list').prev().prev().removeClass('li-header-right').addClass('li-header-down');
+				$(this).parents('.ps-lv3-list').show();
+
+				// show level 2 list
+				$(this).parents('.ps-lv2-list').prev().prev().removeClass('li-header-right').addClass('li-header-down');
+				$(this).parents('.ps-lv2-list').show();
 			}
 		});
 	}
@@ -36,16 +50,15 @@ function init_ps(level, mtype_id, stype_id, p_id){
 
 $(document).ready(function(){
 
-	// show & hide
+	// show list
 	$(document.body).on('click', '.li-header-right', function(){
-		$(this).removeClass('li-header-right');
-		$(this).addClass('li-header-down');
-		$(this).parent().next().show();
+		$(this).removeClass('li-header-right').addClass('li-header-down');
+		$(this).next().next().show();
 	});
 
+	// hide list
 	$(document.body).on('click', '.li-header-down', function(){
-		$(this).removeClass('li-header-down');
-		$(this).addClass('li-header-right');
-		$(this).parent().next().hide();
+		$(this).removeClass('li-header-down').addClass('li-header-right');
+		$(this).next().next().hide();
 	});
 });
