eb8dd1c1d063bf510b6ca86a14e14793a7c29993
hustlzp <hustlzp@qq.com>
2013-05-07 19:40:31 +0800

    finish customer contact function & use mailgun to send mail

--- optico/controllers/home.py
+++ optico/controllers/home.py
@@ -1,10 +1,13 @@
 #-*- coding: UTF-8 -*-
 
+import smtplib
+from email.mime.text import MIMEText
 from flask import render_template, request, redirect, url_for, json
 from optico import app
 from optico.models.type_model import Type
 from optico.models.carousel_model import Carousel
 from optico.utils import convert_dict
+import config
 
 # page home
 #--------------------------------------------------
@@ -37,6 +40,30 @@ def about():
 #--------------------------------------------------
 
 # view (public)
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-	return render_template('contact.html')
+	if request.method == 'GET':
+		return render_template('contact.html')
+	else:
+		name = request.form['name']
+		email = request.form['email']
+		company = request.form['company']
+		comments = request.form['comments']
+
+		# prepare email content
+		msgText = '''<html>
+			<p>Name: %s</p>
+			<p>Email: %s</p>
+			<p>Company: %s</p>
+			<p>Comments: %s</p>
+			</html>''' % (name, email, company, comments)
+		msg = MIMEText(msgText, 'html', 'utf-8')
+		msg['From'] = "%s<%s>" % (name, email)
+		msg['To'] = "sales<sales@optico.com.cn>" 
+		msg['Subject'] = "Customer Contact"
+
+		# send email
+		s = smtplib.SMTP(config.SMTP_SERVER, config.SMTP_PORT)
+		s.login(config.SMTP_USER, config.SMTP_PASSWORD)
+		s.sendmail(config.SMTP_USER, 'hustlzp@optico.com.cn', msg.as_string())
+		return redirect(url_for('home'))
