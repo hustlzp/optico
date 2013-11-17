c74cdc13a7f6f340b6456fb42809655cabaeb7e0
hustlzp <hustlzp@qq.com>
2013-11-17 21:22:59 +0800

    complete refact of carousel controller

--- optico/controllers/carousel.py
+++ optico/controllers/carousel.py
@@ -1,65 +1,56 @@
 #-*- coding: UTF-8 -*-
 import os
-from flask import render_template, request, redirect, url_for, json
-from optico import app, images
+from flask import render_template, request, redirect, url_for
+from optico import app, db, images
 import config
 from optico.models import Carousel
-from optico.utils import check_admin, build_cimg_filename
+from optico.utils import check_admin
 
-# Page manage carousel
-#--------------------------------------------------
 
-# View (admin)
 @app.route('/carousel/manage', methods=['GET', 'POST'])
 def manage_carousel():
+    """Page - manage carousel"""
     check_admin()
     if request.method == 'GET':
+        max_id = db.session.query(db.func.max(Carousel.id).label('max_id')).one().max_id
         carousels = Carousel.query.all()
-        return render_template('carousel/manage.html', carousels=carousels)
+        return render_template('carousel/manage.html', carousels=carousels, max_id=max_id)
     else:
+        # Save image
+        max_id = db.session.query(db.func.max(Carousel.id).label('max_id')).one().max_id
+        filename = images.save(request.files['image'], name='c%s.' % str(max_id + 1))
+
         # add carousel
-        link = request.form['link_url']
-        title = request.form['title']
-        content = request.form['content']
-        new_id = Carousel.add(link, title, content)
-
-        # save image
-        image = request.files['image']
-        image_filename = build_cimg_filename(new_id, image.filename)
-        image.save(config.IMAGE_PATH + image_filename)
-
-        # update carousel image url
-        image_url = config.IMAGE_URL + image_filename
-        Carousel.update_img_url(new_id, image_url)
+        carousel = Carousel(title=request.form['title'], content=request.form['title'], link_url=request.form['title'],
+                            image=filename)
+        db.session.add(carousel)
+        db.session.commit()
         return redirect(url_for('manage_carousel'))
 
-# Page edit carousel
-#--------------------------------------------------
 
-# View (admin)
 @app.route('/carousel/<int:c_id>/edit', methods=['GET', 'POST'])
 def edit_carousel(c_id):
+    """Page - edit carousel"""
     check_admin()
+    carousel = Carousel.query.get_or_404(c_id)
     if request.method == 'GET':
-        carousel = Carousel.get_by_id(c_id)
         return render_template('carousel/edit.html', carousel=carousel)
     else:
-        # save image
+        # Delete old images
+        # TODO - because upload sets has no delele method
+
+        # Save new image if uploaded
         image = request.files['image']
         if image.filename:
-            # get the original image file name
-            image_filename = build_cimg_filename(c_id, image.filename)
-            image.save(config.IMAGE_PATH + image_filename)
-            image_url = config.IMAGE_URL + image_filename
-        else:
-            image_url = Carousel.get_by_id(c_id)['ImageUrl']
+            filename = images.save(image, name='c%s.' % str(carousel.id))
+            carousel.image = filename
 
-        # Edit carousel
-        link = request.form['link']
-        title = request.form['title']
-        content = request.form['content']
-        Carousel.edit(c_id, image_url, link, title, content)
-
+        # update carousel info
+        carousel.link_url = request.form['link_url']
+        carousel.title = request.form['title']
+        carousel.content = request.form['content']
+        db.session.add(carousel)
+        db.session.commit()
         return redirect(url_for('manage_carousel'))
 
 # Page delete carousel
@@ -68,13 +59,13 @@ def edit_carousel(c_id):
 # View (admin)
 @app.route('/carousel/<int:c_id>/delete')
 def delete_carousel(c_id):
+    """Proc - delete carousel"""
     check_admin()
 
-    # Try to delete img file
-    image_filename = Carousel.get_by_id(c_id)['ImageUrl'].split('/')[-1]
-    image_path = config.IMAGE_PATH + image_filename
-    if os.path.isfile(image_path):
-        os.remove(image_path)
+    # Try to delete image file
+    # TODO
 
-    Carousel.delete(c_id)
+    carousel = Carousel.query.get_or_404(c_id)
+    db.session.delete(carousel)
+    db.session.commit()
     return redirect(url_for('manage_carousel'))
